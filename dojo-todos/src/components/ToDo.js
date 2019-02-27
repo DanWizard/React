a63aa7042c1ece06a73f51deb777f849cc2e18ae
Daniel Warrick <42007188+DanWizard@users.noreply.github.com>
2019-02-27 11:47:04 -0600

    optimized adding and deleting and am able to display

--- dojo-todos/src/components/ToDo.js
+++ dojo-todos/src/components/ToDo.js
@@ -8,7 +8,9 @@ class ToDo extends Component {
 		this.state = {
 			todo: '',
 			todos: [],
-      completed_todos: []
+      // completed_todos: [],
+      // active_todos: [],
+      phase: 'all'
 		}
 		this.handleChange = this.handleChange.bind(this)
 		this.createToDo = this.createToDo.bind(this)
@@ -16,12 +18,16 @@ class ToDo extends Component {
     this.completedToDo = this.completedToDo.bind(this)
 	}
 
+  clearComplete(){
+    this.setState({ todos: this.state.todos.filter((td) => td.status !== 'complete')})
+  }
+
+
   removeToDo(evt, index){
-    const list = Object.assign([], this.state.todos)
-    list.splice(index, 1);
     this.setState({
       todos: this.state.todos.filter((td, Tindex) => Tindex !== index),
-      completed_todos: this.state.completed_todos.filter((cd) => cd.index !== index)
+      // completed_todos: this.state.completed_todos.filter((cd) => cd.index !== index),
+      // active_todos: this.state.todos.filter((at) => at.status !== 'complete')
     })
   }
 
@@ -29,25 +35,33 @@ class ToDo extends Component {
     const todos = Object.assign([], this.state.todos)
     if(todos[index].status == 'incomplete'){
       todos[index].status = 'complete'
-      todos[index].index = index
-      let todo = todos[index]
-      console.log('todo', todo)
+      // todos[index].index = index
+      // let todo = todos[index]
+      // console.log('todo', todo)
       
-      console.log("todo",todos[index])
-      this.setState({todos: todos, completed_todos:this.state.completed_todos.concat(todo)})
-      console.log("completed_todo",this.state.completed_todos)
+      // console.log("todo",todos[index])
+      this.setState({
+        todos: todos, 
+        // completed_todos:this.state.completed_todos.concat(todo),
+        // active_todos: this.state.todos.filter((at) => at.status !== 'complete')
+      })
+      // console.log("completed_todo",this.state.completed_todos)
     }
     else{
-      console.log('hello')
+      // const active = Object.assign([], this.state.completed_todos.filter((ct)=> index == ct.index ))
+      // active.status = 'incomplete'
+      // console.log("active", active)
       let newStatus = 'incomplete'
       this.setState({
-        completed_todos: this.state.completed_todos.filter((ct)=>  index !== ct.index ),
+        // completed_todos: this.state.completed_todos.filter((ct)=>  index !== ct.index ),
         todos: this.state.todos.map((td, Tindex) => {
           if(Tindex !== index) return td;
           return {...td, status: newStatus}
-        })
+        }),
+      }, 
 
-      })
+      // this.setState({active_todos: this.state.todos.filter((at) => at.status !== 'complete') })
+      )
       
     }
   }
@@ -57,7 +71,11 @@ class ToDo extends Component {
 		let list = Object.assign([], this.state.todos)
 		let todo = Object.assign({}, this.state.todo)
 		list.push(todo)
-		this.setState({todos: list, todo:{name:'', status:''}})
+		this.setState({
+      todos: list, 
+      todo:{name:'', status:''},
+      // active_todos: list
+    })
     console.log(this.state.todo)
 	}
 
@@ -65,32 +83,85 @@ class ToDo extends Component {
 		this.setState({todo: {name:event.target.value, status:'incomplete'}});
 	}
 
+  changePhase(num){
+      console.log('hello')
+      if(num == 1){
+        this.setState({phase: 'all'})
+      }
+      if(num == 2){
+        this.setState({phase: 'active'})
+      }
+      if(num == 3){
+        this.setState({phase: 'complete'})
+      }
+  }
 
   	render() {
+        const phase = this.state.phase
+
+        let content, count, clear
+
+        if(phase == 'all'){
+          count = 
+          <div className='col text-center'>
+              <p className='link'>{this.state.todos.length} todos</p>
+          </div>
+          content = 
+              this.state.todos.map((todo, index)=>{
+                return(
+                  <Cell complete={this.completedToDo} status={todo.status} text={todo.name} remove={this.removeToDo} num={index}/>
+                )
+              })
+          clear = 
+          <div className='col text-center'>
+              <p className='link' onClick={() => this.clearComplete()}>clear completed</p>
+          </div>
+        }
+        if(phase == 'active'){
+          content = 
+              this.state.todos.map((todo, index)=>{
+                if(todo.status == 'incomplete'){
+                return(
+                  <Cell complete={this.completedToDo} status={todo.status} text={todo.name} remove={this.removeToDo} num={index}/>
+                )
+              }
+              })
+        }
+        if(phase == 'complete'){
+          content = 
+              this.state.todos.map((todo, index)=>{
+                if (todo.status == 'complete'){
+                  return(
+                  <Cell complete={this.completedToDo} status={todo.status} text={todo.name} remove={this.removeToDo} num={index}/>
+                  )
+                }
+                
+              })
+          clear = 
+          <div className='col text-center'>
+              <p className='link' onClick={() => this.clearComplete()}>clear completed</p>
+          </div>
+        }
+
   	  	return (
   	  	  <div className="row">
   	  	  	<div className="col co-form">
   	  	  		<form onSubmit={this.createToDo}>
   	  	  			<input onChange={this.handleChange} className="create" type='text' value={this.state.todo.name} placeholder="Add ToDo"/>
   	  	  		</form>
-  	  	  		{this.state.todos.map((todo, index)=>{
-  	  	  			return(
-						      <Cell complete={this.completedToDo} status={todo.status} text={todo.name} remove={this.removeToDo} num={index}/>
-  	  	  			)
-  	  	  		})}
+  	  	  		 {content}
   	  	  		<div className= 'row'>
+  	  	  			{count}
   	  	  			<div className='col text-center'>
-  	  	  				<p></p>
+  	  	  				<p className='link' onClick={() => this.changePhase(1)}>All</p>
   	  	  			</div>
   	  	  			<div className='col text-center'>
-  	  	  				<a className='link' href=''>All</a>
+  	  	  				<p className='link' onClick={() => this.changePhase(2)}>Active</p>
   	  	  			</div>
   	  	  			<div className='col text-center'>
-  	  	  				<a className='link' href=''>Active</a>
+  	  	  				<p className='link' onClick={() => this.changePhase(3)}>Completed</p>
   	  	  			</div>
-  	  	  			<div className='col text-center'>
-  	  	  				<a className='link' href=''>Completed</a>
-  	  	  			</div>
+                {clear}
   	  	  		</div>
   	  	  	</div>
   	  	  </div>
